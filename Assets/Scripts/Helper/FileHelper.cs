a78fcffb6137d07405af11f4d290276b334fc044
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-19 16:27:05 +0300

    added filehelper for exporting levels

---
+++ Assets/Scripts/Helper/FileHelper.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.IO;
+
+public class FileHelper  {
+	public static void SaveStringToFile(string path,string content)
+	{
+		StreamWriter writer = new StreamWriter (path);
+		writer.Write (content);
+		writer.Close ();
+
+	}
+}
