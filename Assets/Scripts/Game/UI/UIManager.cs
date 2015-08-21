53acaafec3f0d583c529261d95de87b5c820b7d9
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-21 11:53:26 +0300

    added ui manager

---
+++ Assets/Scripts/Game/UI/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager  {
+
+
+	public static void ShowLevelClearedMenu(bool show)
+	{
+		GameObject.Find ("White Background").SetActive (show);
+		GameObject.Find ("Game Over Panel").SetActive (show);
+	}
+}
