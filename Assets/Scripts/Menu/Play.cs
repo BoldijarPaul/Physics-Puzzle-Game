bf374d633604fc977c0c14b7e22ca1e915922ab7
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-21 14:45:15 +0300

    update

---
+++ Assets/Scripts/Menu/Play.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour {
+
+
+	void OnMouseUp()
+	{
+		Debug.Log ("test");
+		Application.LoadLevel ("GameScene");
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
