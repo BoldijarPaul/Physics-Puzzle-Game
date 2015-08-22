1ef2ddd3eee441b4fcfdc85ed8d3090a7ea27f3d
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-22 14:44:31 +0300

    working at scroll and creating levels at runtime

---
+++ Assets/Scripts/Menu/MenuPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPanel : MonoBehaviour {
+
+
+	void Start () {
+	
+		GameObject levelBox=Resources.Load<GameObject> ("Prefabs/Level Box");
+
+		int y = 0;
+		for (int i=1; i<=10; i++) {
+			GameObject a = (GameObject)Instantiate (levelBox); 
+			a.transform.SetParent (transform, false);
+			RectTransform rectTransform=a.GetComponent<RectTransform>();
+			 
+			rectTransform.anchoredPosition=new Vector2(0,y);
+			y-=300;
+
+
+			if(Random.Range(-10,10)<0)
+			{
+				a.GetComponent<UnlockedLevelBox>().setData(false,Random.Range(1,99));
+			}
+			else
+			{
+				a.GetComponent<UnlockedLevelBox>().setData(true,0);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Menu/UnlockedLevelBox.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UnlockedLevelBox : MonoBehaviour {
+
+ 
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void setData(bool locked,int level)
+	{
+		Image image = GetComponent<RectTransform> ().GetChild (1).GetComponent<Image> ();
+		Text text = GetComponent<RectTransform> ().GetChild (0).GetComponent<Text> ();
+		if (locked) {
+			text.text = "";
+			image.color = new Color (image.color.r, image.color.g, image.color.b, 1);
+		} else {
+			image.color = new Color (image.color.r, image.color.g, image.color.b, 0);
+			text.text = level + "";
+		}
+
+	}
+}
